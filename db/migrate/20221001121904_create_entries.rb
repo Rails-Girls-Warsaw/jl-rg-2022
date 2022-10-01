43e6e221cc2d4c2649ffdc9be7d1a84cb20bf909
Jarek L <weezing@protonmail.com>
2022-10-01 14:58:15 +0000

    Add entries index page

---
+++ db/migrate/20221001121904_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.string :header
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_01_121904) do
+  create_table "entries", force: :cascade do |t|
+    t.string "title"
+    t.string "header"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
